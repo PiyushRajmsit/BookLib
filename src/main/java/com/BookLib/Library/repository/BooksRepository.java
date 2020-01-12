95d61ad32aac5a7cd062f5ae546e492c3c4765bf
Piyush Raj <piyraj007@gmail.com>
2020-01-12 16:31:35 +0530

    No changes made

--- src/main/java/com/BookLib/Library/repository/BooksRepository.java
+++ src/main/java/com/BookLib/Library/repository/BooksRepository.java
@@ -3,7 +3,6 @@ package com.BookLib.Library.repository;
 import com.BookLib.Library.model.Books;
 import com.BookLib.Library.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface BooksRepository extends JpaRepository<Books,Long> {
